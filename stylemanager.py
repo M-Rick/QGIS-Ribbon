b2a577cb836d0b2b9896be50492b7e14645cf8ab
M-Rick <m.rick.mac@gmail.com>
2021-09-23 19:14:19 +0200

    Add files via upload

---
+++ stylemanager.py
@@ -0,0 +1,7 @@
+import os
+from qgis.PyQt import uic
+from qgis.PyQt.QtWidgets import QDialog, QFileDialog, QInputDialog, QMessageBox
+from qgis.PyQt.QtCore import Qt, QCoreApplication
+from ..utils import tr
+from ..utils import DEFAULT_STYLE
+
