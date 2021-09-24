e4d59d1a19dd9f403422ced7e76f882b6340c42e
M-Rick <m.rick.mac@gmail.com>
2021-09-24 12:59:03 +0200

    Add files via upload

---
+++ i18n/rib_fr.ts
@@ -0,0 +1,324 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../config.py" line="20"/>
+        <source>Failed to load confing.json.</source>
+        <translation>Chargement du fichier de configuration config.json échoué.</translation>
+    </message>
+    <message>
+        <location filename="../dynamic_layout.py" line="44"/>
+        <source>New tab</source>
+        <translation>Nouvel onglet</translation>
+    </message>
+    <message>
+        <location filename="../dynamic_layout.py" line="105"/>
+        <source>New section</source>
+        <translation>Nouvelle section</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="6"/>
+        <source>Main Tools</source>
+        <translation>Outils principaux</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="10"/>
+        <source>Project</source>
+        <translation>Projet</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="21"/>
+        <source>Navigation</source>
+        <translation>Navigation</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="37"/>
+        <source>Attributes</source>
+        <translation>Attributs</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="48"/>
+        <source>Measurement</source>
+        <translation>Mesure</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="58"/>
+        <source>Add Layer</source>
+        <translation>Ajouter un calque</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="81"/>
+        <source>Create Layer</source>
+        <translation>Créer un calque</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="118"/>
+        <source>Advanced Tools</source>
+        <translation>Outils avancés</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="142"/>
+        <source>Labels</source>
+        <translation>Étiquettes</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="158"/>
+        <source>Vector</source>
+        <translation>Vecteur</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="177"/>
+        <source>Digitizing</source>
+        <translation>Numérisation</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="94"/>
+        <source>Prints</source>
+        <translation>Impression</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="130"/>
+        <source>Project</source>
+        <translation>Projet</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="142"/>
+        <source>Navigation</source>
+        <translation>Navigation</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="159"/>
+        <source>Attributes</source>
+        <translation>Attributs</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="171"/>
+        <source>Measurement</source>
+        <translation>Mesure</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="182"/>
+        <source>Add Layer</source>
+        <translation>Ajouter un calque</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="205"/>
+        <source>Create Layer</source>
+        <translation>Créer un calque</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="218"/>
+        <source>Advanced attributes</source>
+        <translation>Attributs avancés</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="239"/>
+        <source>Labels</source>
+        <translation>Étiquettes</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="256"/>
+        <source>Vector</source>
+        <translation>Vecteur</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="276"/>
+        <source>Vector digitization</source>
+        <translation>Numérisation vectorielle</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="303"/>
+        <source>Prints</source>
+        <translation>Impression</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="326"/>
+        <source>Geoprocessing Tools</source>
+        <translation>Outils de géotraitement</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="343"/>
+        <source>Geometry Tools</source>
+        <translation>Outils de géométrie</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="363"/>
+        <source>Analysis Tools</source>
+        <translation>Outils d'analyse</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="379"/>
+        <source>Research Tools</source>
+        <translation>Outils de recherche</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="398"/>
+        <source>Data Management Tools</source>
+        <translation>Outils de gestion de données</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="411"/>
+        <source>Raster</source>
+        <translation>Raster</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="422"/>
+        <source>Raster analysis</source>
+        <translation>Analyse</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="444"/>
+        <source>Projections</source>
+        <translation>Projections</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="455"/>
+        <source>Miscellaneous</source>
+        <translation>Divers</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="468"/>
+        <source>Extract Projection</source>
+        <translation>Extraction </translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="479"/>
+        <source>Conversion</source>
+        <translation>Conversion</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="492"/>
+        <source>Data base</source>
+        <translation>Base de données</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="500"/>
+        <source>Digitizing</source>
+        <translation>Numérisation</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="521"/>
+        <source>Selection</source>
+        <translation>Sélection</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="539"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="547"/>
+        <source>Plugins</source>
+        <translation>Extensions</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="555"/>
+        <source>Data Source Manager</source>
+        <translation>Gestionnaire de base de données</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="568"/>
+        <source>Advanced digitizing tools</source>
+        <translation>Outils de numérisation avancés</translation>
+    </message>
+
+    <message>
+        <location filename="../layout.py" line="78"/>
+        <source>Switch on manually missing core plugin: Topology Checker</source>
+        <translation>Włącz brakującą wtyczkę wbudowaną: Kontrola Topologii</translation>
+    </message>
+    <message>
+        <location filename="../layout.py" line="380"/>
+        <source>Please, restart QGIS!</source>
+        <translation>Veuillez redémarrer QGIS !</translation>
+    </message>
+    <message>
+        <location filename="../layout.py" line="134"/>
+        <source>Enter the data according to the scheme:</source>
+        <translation>Wpisz dane według schematu:</translation>
+    </message>
+
+    <message>
+        <location filename="../layout.py" line="135"/>
+        <source>for the address point:</source>
+        <translation>dla punktu adresowego:</translation>
+    </message>
+
+    <message>
+        <location filename="../layout.py" line="136"/>
+        <source>for the street:</source>
+        <translation>dla ulicy:</translation>
+    </message>
+    <message>
+        <location filename="../layout.py" line="105"/>
+        <source>Show menu</source>
+        <translation>Montrer le menu</translation>
+    </message>
+    <message>
+        <location filename="../layout.py" line="111"/>
+        <source>Edit menu</source>
+        <translation>Modifier</translation>
+    <message>
+        <location filename="../layout.py" line="399"/>
+        <source>Finish edition</source>
+        <translation>Termminer la modification</translation>
+    </message>
+    <message>
+        <location filename="../dynamic_layout.py" line="132"/>
+        <source>Do you want to save your changes?</source>
+        <translation>Voulez-vous enrergistrer les modifications ?</translation>
+    </message>
+    <message>
+    <location filename="../dynamic_layout.py" line="173"/>
+    <source>Edit tools within a section:</source>
+    <translation>Modifier un outil d'une section :</translation>
+    </message>
+    <message>
+        <location filename="../dynamic_layout.py" line="173"/>
+    <source>Moving tools</source>
+    <translation>Déplacement d'un outil</translation>
+    </message>
+    <message>
+            <location filename="../dynamic_layout.py" line="173"/>
+    <source>click and hold the left mouse button</source>
+    <translation>Cliquer et maintenir le bouton gauche de la souris</translation>
+    </message>
+    <message>
+    <location filename="../dynamic_layout.py" line="173"/>
+    <source>on the icon and move it to the desired location</source>
+    <translation>przyciskiem myszy ikony i przesunięcie w wybrane miejsce</translation>
+    </message>
+    <message>
+    <location filename="../dynamic_layout.py" line="173"/>
+    <source>Tool removal</source>
+    <translation>Supprimer un outil</translation>
+    </message>
+    <message>
+    <location filename="../dynamic_layout.py" line="173"/>
+    <source>double click</source>
+    <translation>double clique</translation>
+    </message>
+    <message>
+    <location filename="../dynamic_layout.py" line="173"/>
+    <source>tool icon and click delete button</source>
+    <translation>ikony narzędzia i przycisk Delete</translation>
+    </message>
+    <message>
+        <location filename="../dynamic_layout.py" line="288"/>
+        <source>Select item has sub-section</source>
+        <translation>Wybrany element ma podsekcję</translation>
+    </message>
+    <message>
+        <location filename="../dynamic_layout.py" line="867"/>
+        <source>Composition settings</source>
+        <translation>Ustawienia kompozycji</translation>
+    </message>
+    <message>
+        <location filename="../ribbon_config.py" line="122"/>
+        <source>Advanced attributes</source>
+        <translation>Attributs avancés</translation>
+    </message>
+</context>
+</TS>
